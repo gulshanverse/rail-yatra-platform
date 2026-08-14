9bf03422a934f7ac93b0365e174c4990a0e708ac
Gulshan Kumar <gulshankumar41309@gmail.com>
2026-08-14 17:47:30 +0530

    feat(ui): export home components

---
+++ apps/frontend/src/components/home/index.ts
@@ -0,0 +1,6 @@
+export { HomeIntelligence } from './HomeIntelligence';
+export { HomeTrustStatus } from './HomeTrustStatus';
+export { JourneyComposer } from './JourneyComposer';
+export { RecentJourneyCard } from './RecentJourneyCard';
+export { RecentJourneys } from './RecentJourneys';
+export type { RecentJourney } from './RecentJourneyCard';
